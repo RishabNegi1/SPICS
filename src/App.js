067eacd6e841706c605f4c50b9096114140c99aa
RishabNegi1 <rishabnegi108@gmail.com>
2023-08-14 21:23:32 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './Components/Main';
 import Navbar from './Components/Navbar';
 import Upload from './Pages/Upload';
 
 function App() {
+  const [uploadedPhotos, setUploadedPhotos] = useState([]);
+
   const handlePhotoAdd = (photos) => {
-    // Handle the list of photos here, e.g., store it in state or send it to the server.
-    console.log('Uploaded Photos:', photos);
-  };
+    setUploadedPhotos([...uploadedPhotos, ...photos]);
+  }
   return (
     <div className="App">
        <BrowserRouter>
       <Navbar/>
       <Routes>
-          <Route path='/' element={<><Main/></>} />
-          <Route path='/up' element={<Upload onPhotoAdd={handlePhotoAdd}/>} />
-        </Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/up' element={<Upload onPhotoAdd={handlePhotoAdd}/>} />
+      </Routes>
         
       </BrowserRouter>
     </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -24,12 +24,12 @@ const Main = ({ photos }) => {
       <div className='font-serif'>
       <h1 className=' font-bold text-2xl md:text-3xl underline mt-10'>GALLERY</h1>
         <div className=' mt-10 max-w-[1360px] mx-auto lg:grid grid-cols-3 grid-rows-2 lg:w-[70%] md:w-[60%] sm:w-[70%] items-center'>
-        {photos.map((photo, index) => (
+        {photos?.map((photo, index) => (
         <img
           className=' w-[100%] h-[100%] object-cover'
           key={index}
           src={photo.preview}
-          alt={`Photo ${index + 1}`}
+          alt={`Photo ${index}`}
         />
       ))}
     </div>
